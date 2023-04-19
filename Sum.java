ac27088aaf369fedc6210e04f18c1577fc0cb02b
Navneet Mehta <navneetmehta6202@gmail.com>
2023-04-19 13:11:29 +0530

    Sum.java is modified

--- Sum.java
+++ Sum.java
@@ -4,6 +4,8 @@ public class Sum {
     public static void main(String[] args) {
         int a=10;
         int b=100;
+        int c=300;
         System.out.println("The sum is:"+(a+b));
+        System.out.println("The sum is:"+(a+b+c));
     }
 }
