8ac2323e6ca5b7ebf25344448572f569cc840e9d
Navneet Mehta <navneetmehta6202@gmail.com>
2023-04-19 12:27:15 +0530

    Diff.java is added and sum.java is modified

---
+++ Diff.java
@@ -0,0 +1,9 @@
+package Git.java;
+
+public class Diff {
+    public static void main(String[] args) {
+        int a=20;
+        int b=30;
+        System.out.println("The diff is :"+(b-a));
+    }
+}
--- Sum.java
+++ Sum.java
@@ -4,6 +4,6 @@ public class Sum {
     public static void main(String[] args) {
         int a=10;
         int b=100;
-        System.out.println(a+b);
+        System.out.println("The sum is:"+(a+b));
     }
 }
